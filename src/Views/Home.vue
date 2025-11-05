0277bad1a5874282b3cce0e8b7cce7ad60875783
David-533 <david.ramin.pro@gmail.com>
2025-11-05 10:58:58 +0100

    Fix typo in navigation link from 'Profil' to 'Profile'

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -8,7 +8,7 @@
       <!-- Liens de navigation -->
       <div class="nav-links">
         <router-link to="/home" class="nav-item">Accueil</router-link>
-        <router-link to="/profil" class="nav-item">Profil</router-link>
+        <router-link to="/profil" class="nav-item">Profile</router-link>
         <router-link to="/messages" class="nav-item">Messages</router-link>
 
       </div>
